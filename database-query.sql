875c637e7462c1b90c307257b0512fed9f0b8e01
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-05-16 03:34:20 -0300

    Added a script for create test database and use a beforeEach method for truncate the table

--- database-query.sql
+++ database-query.sql
@@ -1,4 +1,4 @@
-DROP SCHEMA IF EXISTS claro_brasil_nodejs;
+DROP SCHEMA IF EXISTS `claro_brasil_nodejs_test`;
 
 CREATE SCHEMA `claro_brasil_nodejs` ;
 
@@ -11,5 +11,18 @@ CREATE TABLE `device` (
     model TEXT
 );
 
+-- TEST DATABASE
+--DROP SCHEMA IF EXISTS `claro_brasil_nodejs_test`;
+
+--CREATE SCHEMA `claro_brasil_nodejs_test` ;
+
+--USE `claro_brasil_nodejs_test`;
+
+/*CREATE TABLE `device` (
+    id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    name TEXT,
+    model TEXT
+); */
 --INSERT INTO device (user_id, name, model) VALUES (1, 'IPHONE SE (GSM)', 'IOS');
 --INSERT INTO device (user_id, name, model) VALUES (1, 'SM-G920F', 'ANDROID');
--- test/integartion/services/database/device/devices-test.js
+++ test/integartion/services/database/device/devices-test.js
@@ -1,6 +1,20 @@
 const test = require('ava')
-const {db} = require('../../../setup')
+
+require('dotenv').config()
+
+const db = require('knex')({
+  client: process.env.DATABASE_CLIENT,
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_SCHEMA_TEST
+  }
+})
+
 const devices = require('../../../../../src/services/database/device')(db)
+
+test.beforeEach((t) => db('device').truncate())
 
 test('Should create a device', async (t) => {
   const newDevice = await devices
