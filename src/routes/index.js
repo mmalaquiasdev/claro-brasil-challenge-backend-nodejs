8ae9e55047bea0c76253f391245933519e87e6f6
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-05-16 01:29:51 -0300

    Finish the new crud implementation using knex for device model, it's light, fast and more modular

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,56 @@
 const db = require('../services/database')
 
 const routes = (server) => {
-  server.get('/users/:user_id/devices', (req, res) => {
-    return db
-      .device()
-      .findAll()
-      .then(data => res.json(({ devices: data })))
+  server.get('/users/:user_id/devices', async (req, res) => {
+    try {
+      const devices = await db.device().findAll()
+      return res.send(devices)
+    } catch (error) {
+      console.error(error)
+      return res.send(error)
+    }
   })
-  // server.get('/users/:user_id/devices', devicesHandlerFindAll)
-  // server.get('/users/:user_id/devices/:id', devicesHandlerFindOne)
-  // server.post('/users/:user_id/devices', devicesHandlerCreate)
-  // server.patch('/users/:user_id/devices/:id', devicesHandlerUpdateName)
-  // server.del('/users/:user_id/devices/:id', devicesHandlerDelete)
+
+  server.get('/users/:user_id/devices/:device_id', async (req, res) => {
+    try {
+      const device = await db.device().findOne(req.params)
+      return res.send(device)
+    } catch (error) {
+      console.error(error)
+      return res.send(error)
+    }
+  })
+
+  server.post('/users/:user_id/devices', async (req, res) => {
+    try {
+      const newDevice = await db.device().create(req.body)
+      return res.send(newDevice)
+    } catch (error) {
+      console.error(error)
+      return res.send(error)
+    }
+  })
+
+  server.patch('/users/:user_id/devices/:device_id', async (req, res) => {
+    try {
+      const updatedDevice = await db.device().update(req.params, req.body)
+      return res.send(updatedDevice)
+    } catch (error) {
+      console.error(error)
+      return res.send(error)
+    }
+  })
+
+  server.del('/users/:user_id/devices/:device_id', async (req, res) => {
+    try {
+      await db.device().del(req.params)
+      res.status(204)
+      return res.send()
+    } catch (error) {
+      console.error(error)
+      return res.send(error)
+    }
+  })
 }
 
 module.exports = routes
--- src/services/database/device/index.js
+++ src/services/database/device/index.js
@@ -1,14 +1,59 @@
 const deviceFactory = (db) => ({
   findAll: () => findAll(db),
-  findOne: () => {},
+  findOne: (params) => findOne(db, params),
   count: () => {},
-  create: () => {},
-  update: () => {},
-  del: () => {}
+  create: (body) => create(db, body),
+  update: (params, body) => update(db, params, body),
+  del: (params) => del(db, params)
 })
 
 const findAll = (db) => {
   return db.select().table('device')
+}
+
+const findOne = (db, params) => {
+  return db
+    .first()
+    .table('device')
+    .where({
+      id: params.device_id,
+      user_id: params.user_id
+    })
+}
+
+const create = (db, body) => {
+  return db
+    .into('device')
+    .insert(body)
+    .then((newId) => {
+      const params = {
+        device_id: newId,
+        user_id: body.user_id
+      }
+
+      return findOne(db, params)
+    })
+}
+
+const update = (db, params, body) => {
+  return db('device')
+    .update({
+      name: body.name
+    })
+    .where({
+      id: params.device_id,
+      user_id: params.user_id
+    })
+    .then(() => findOne(db, params))
+}
+
+const del = (db, params) => {
+  return db('device')
+    .del()
+    .where({
+      id: params.device_id,
+      user_id: params.user_id
+    })
 }
 
 module.exports = deviceFactory
